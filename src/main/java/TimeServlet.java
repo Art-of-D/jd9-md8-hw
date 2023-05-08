aa84eab5602c1a3c3536effc23342ff9794414bf
Artur Didur <didur.art@gmail.com>
2023-05-08 10:51:40 +0200

    fourth
    added WebFilter

--- src/main/java/TimeServlet.java
+++ src/main/java/TimeServlet.java
@@ -15,12 +15,8 @@ public class TimeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=utf-8");
-        if (new TimezoneValidateFilter().checkV(req)){
-            resp.getWriter().write(timeChecker(req));
-            resp.getWriter().close();
-        } else {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid timezone");
-        }
+        resp.getWriter().write(timeChecker(req));
+        resp.getWriter().close();
     }
 
     protected String timeChecker(HttpServletRequest req){
